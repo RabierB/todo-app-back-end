2292b51f3128f6fbfb3b0e4e75d0470ea16959b9
Rabier <rbenabdalla@cps.edu>
2018-10-20 18:31:55 +0000

    starting dynamics

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,6 +2,12 @@ class TodoController < ApplicationController
     def index
     end
     
-        def show 
+    def show 
+        if params[:id] == "1"
+            @title = "Make the Curriculum"
         end
+        if params[:id] == "2"
+            @title = "Buy Workshop Supplies"
+        end
+    end
 end
