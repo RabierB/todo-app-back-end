169a27356209c7c5eec01f30b112ac01635d10e7
Rabier <rbenabdalla@cps.edu>
2018-10-20 19:11:29 +0000

    refactoring

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,31 +3,32 @@ class TodoController < ApplicationController
     end
     
     def show 
-        if params[:id] == "1"
+        id = params[:id]
+        if id == "1"
             @title = "Make the Curriculum"
             @size = "3"    
-        end
-        if params[:id] == "2"
+        
+        elsif id == "2"
             @title = "Buy Workshop Supplies"
             @size = "2"
-        end
-        if params[:id] == "3"
+        
+        elsif id == "3"
             @title = "Meet with the volunteer trainers"
             @size = 2
-        end
-        if params[:id] == "4"
+        
+        elsif id == "4"
             @title = "Order food for Saturday and Sunday"
             @size = 1
-        end
-        if params[:id] == "5"
+        
+        elsif id == "5"
             @title = "Check pre-work assignments"
             @size = "2"
-        end 
-        if params[:id] == "6"
+         
+        elsif id == "6"
             @title = "Send workshop locations to all students"
             @size = "1"
-        end
-        if params[:id] == "7"
+        
+        elsif id == "7"
             @title = "Have a great workkshop"
             @size = "0"
         end
