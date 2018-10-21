eb50798b63acb519104f009ab8147b41da35f9c0
Rabier <rbenabdalla@cps.edu>
2018-10-21 18:17:44 +0000

    funtional todo list

--- config/routes.rb
+++ config/routes.rb
@@ -64,4 +64,10 @@ get "todo/new", to: "todo#new"
 
 get 'todo/create', to: 'todo#create'
 
+get 'todo/edit/:id', to: 'todo#edit'
+
+get 'todo/update/:id', to: 'todo#update'
+
+get 'todo/destroy/:id', to: 'todo#destroy'
+
 end
